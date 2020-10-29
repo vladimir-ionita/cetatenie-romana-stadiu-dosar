a5e96cd492f2be7d1c5fc1986532e9961c0da422
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-29 18:30:22 -0500

    Implement `convert_pdf_to_txt`

--- pdf2txt/converter.py
+++ pdf2txt/converter.py
@@ -1,10 +1,39 @@
 import os
+import shutil
 
 import pdf2image
 from PIL import Image as PILImage
 import pytesseract
 
+import paths
 from . import constants
+
+
+def convert_pdf_to_txt(pdf_input_file_path, text_output_file_path, verbose=False):
+    """Convert a pdf file to a txt file.
+
+    Parameters:
+        pdf_input_file_path (str): the path for the pdf file.
+        text_output_file_path (str): the path for the text file.
+        verbose (bool): flag to indicate the verbosity.
+    """
+    # Create a temporary folder to store temporary files
+    temporary_folder_path = paths.get_repository_path().joinpath(constants.TEMPORARY_FOLDER_NAME)
+    if not os.path.exists(temporary_folder_path):
+        os.mkdir(temporary_folder_path)
+
+    # Convert the PDF file to images
+    if verbose:
+        print("Converting {} to a text file.".format(pdf_input_file_path))
+    images_paths = convert_pdf_to_images(pdf_input_file_path, temporary_folder_path)
+
+    # Convert images to TXT
+    convert_images_to_txt(images_paths, text_output_file_path)
+
+    # Remove the temporary folder path
+    if verbose:
+        print("Converting cleaning up.")
+    shutil.rmtree(temporary_folder_path)
 
 
 def convert_pdf_to_images(pdf_input_file_path, images_output_folder):

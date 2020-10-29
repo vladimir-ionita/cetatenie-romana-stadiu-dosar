3dc9fa626e87cff32e390058bfa6596e8215baa5
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-28 21:20:25 -0500

    Implement `get_publishings`

--- CetatenieJustRoParser/CetatenieJustRoParser.py
+++ CetatenieJustRoParser/CetatenieJustRoParser.py
@@ -1,6 +1,42 @@
 from bs4 import BeautifulSoup
 
 from CetatenieJustRoParser import constants
+from CetatenieJustRoParser.OrderData import OrderData
+from CetatenieJustRoParser.PublishingData import PublishingData
+
+
+def get_publishings(html):
+    """Retrieves all publishings.
+
+    Parameters:
+        html (str): the html content
+
+    Returns:
+        list of PublishingData: a list of parsed PublishingData
+    """
+    publishings_data_list = []
+
+    # Retrieve the list of publishings
+    publishings_item_list = extract_publishings_list_from_html(html)
+    for item in publishings_item_list:
+        # Retrieve the publishing date
+        publishing_date_string = item.strong.get_text().lstrip().rstrip()
+        # Create the publishing data object
+        publishing_data = PublishingData(publishing_date_string)
+        # Extract order links from the item
+        publishing_order_links = extract_links_from_bs_tag(item)
+        for order_link in publishing_order_links:
+            # Retrieve order data
+            order_name = order_link.get_text()
+            order_link = constants.CETATENIE_JUST_RO_BASE_URL + order_link['href']
+            # Create the order data object
+            order_data = OrderData(order_name, order_link)
+            # Append the order to the publishing
+            publishing_data.add_order(order_data)
+        # Store the publishing data
+        publishings_data_list.append(publishing_data)
+
+    return publishings_data_list
 
 
 def extract_publishings_list_from_html(html):

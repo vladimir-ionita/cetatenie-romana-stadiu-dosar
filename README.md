fb5d7df770d205eb87f07a54317510c2df1b1bad
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-29 18:23:29 -0500

    Implement `convert_pdf_to_images`

---
+++ README.md
@@ -0,0 +1,5 @@
+## Libraries
+### pdf2image 
+The `pdf2image` library requires `poppler` to be installed, according to its README file: https://github.com/Belval/pdf2image.
+
+You can install it using homebrew: `brew install poppler`.
---
+++ pdf2txt/converter.py
@@ -0,0 +1,29 @@
+import os
+import pdf2image
+
+from . import constants
+
+
+def convert_pdf_to_images(pdf_input_file_path, images_output_folder):
+    """Convert a pdf file into a series of images.
+
+    Parameters:
+        pdf_input_file_path (str): the path for the pdf file.
+        images_output_folder (str): the folder path for the images
+
+    Return:
+        list of str: the list of the resulting images' paths
+    """
+    # Convert PDF to images
+    pdf_images = pdf2image.convert_from_path(pdf_input_file_path, constants.IMAGE_QUALITY_DPI)
+
+    # Store the images
+    pdf_images_paths = []
+    for index, page in enumerate(pdf_images):
+        image_file_name = constants.IMAGE_FILE_NAME_FORMAT.format(index + 1)
+        image_file_path = os.path.join(images_output_folder, image_file_name)
+        page.save(image_file_path, constants.IMAGE_FILE_FORMAT)
+        pdf_images_paths.append(image_file_path)
+
+    # Return the images' paths
+    return pdf_images_paths

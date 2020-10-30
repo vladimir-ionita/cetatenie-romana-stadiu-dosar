8068d9e51e39c80ee75b8f64ad837ab984a51b2d
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 15:30:16 -0500

    Add verbosity to `run` in `main`

--- main.py
+++ main.py
@@ -8,24 +8,40 @@ from OrdersParser.parser import *
 import paths
 
 
-def run():
+def run(verbose=False):
     # Retrieve the html content
+    if verbose:
+        print("Step 1. Retrieve the html content.")
     html_content = web.get_html_content(CETATENIE_JUST_RO_ORDERS_WEB_PAGE_URL)
 
     # Retrieve publishings data
+    if verbose:
+        print("Step 2. Retrieve the publishings.")
     publishings = retrieve_publishings(html_content)
+    if verbose:
+        print("\t{} publishings found".format(len(publishings)))
 
     # Download publishings
-    download_publishings(publishings)
+    if verbose:
+        print("Step 3. Download the publishings.")
+    download_publishings(publishings, verbose)
 
     # Convert the publishings' orders from PDFs to TXTs
+    if verbose:
+        print("Step 4. Convert the publishings.")
     for p in publishings:
-        convert_publishing_orders_from_pdf_to_txt(p)
+        convert_publishing_orders_from_pdf_to_txt(p, verbose)
 
     # Parse publishings
-    dossiers = get_publishings_list_dossiers(publishings)
+    if verbose:
+        print("Step 5. Retrieve the dossiers.")
+    dossiers = get_publishings_list_dossiers(publishings, verbose)
+    if verbose:
+        print("\t{} dossiers found.".format(len(dossiers)))
 
     # Save the dossiers
+    if verbose:
+        print("Step 6. Save the dossiers.")
     dossiers_years = defaultdict(list)
     for d in dossiers:
         dossiers_years[d.year].append(d.number)
@@ -34,8 +50,11 @@ def run():
         dossiers_file_path = paths.get_dossiers_collection_file_path_for_year(k)
         disk.write_items_list_to_file(dossiers_years[k], dossiers_file_path)
 
+    if verbose:
+        print("Done!")
+
 
 if __name__ == '__main__':
     paths.setup()
-    run()
+    run(verbose=True)
     paths.cleanup()

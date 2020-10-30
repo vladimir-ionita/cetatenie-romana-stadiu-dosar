62ab6aae34200f607303adb53b5336f7d19270f0
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 10:41:49 -0500

    Implement converting in `main`

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ def run():
     # Retrieve the html content
     html_content = web.get_html_content(CETATENIE_JUST_RO_ORDERS_WEB_PAGE_URL)
 
-    # Retrieve publishings
+    # Retrieve publishings data
     publishings = retrieve_publishings(html_content)
 
     # Download publishings
     download_publishings(publishings, verbose=True)
+
+    # Convert the orders from PDFs to TXTs
+    for p in publishings:
+        for order in p.orders:
+            convert_pdf_to_txt(paths.get_order_pdf_file_path(order), paths.get_order_txt_file_path(order), verbose=True)
 
 
 if __name__ == '__main__':

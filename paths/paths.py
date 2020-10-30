ca6e0d12498e5e7e2e22e9491f628397d5fd617b
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 14:49:02 -0500

    Implement `cleanup`

--- paths/paths.py
+++ paths/paths.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 from . import constants
 
@@ -8,6 +10,12 @@ def setup():
     get_publishings_storage_folder_path().mkdir(parents=True, exist_ok=True)
     get_temporary_storage_folder_path().mkdir(parents=True, exist_ok=True)
     get_dossiers_storage_folder_path().mkdir(parents=True, exist_ok=True)
+
+
+def cleanup():
+    """Cleanup the temporary storage."""
+    for item in os.listdir(get_temporary_storage_folder_path()):
+        os.remove(item)
 
 
 def get_storage_folder_path():

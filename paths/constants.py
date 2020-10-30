8411702a80154ce84ee021b0284cf3130cf3d04b
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 14:15:52 -0500

    Change `PUBLISHINGS_STORAGE_PATH` to be relative to `FILES_STORAGE_PATH`

--- paths/constants.py
+++ paths/constants.py
@@ -1,2 +1,4 @@
+import os
+
 FILES_STORAGE_PATH = "dev/romanian-citizenship-status-files"
-PUBLISHINGS_STORAGE_PATH = "dev/romanian-citizenship-status-files/publishings"
+PUBLISHINGS_STORAGE_PATH = os.path.join(FILES_STORAGE_PATH, "publishings")

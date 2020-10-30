619ca984e9816126d230911433d95e1fd2c4e4a3
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 13:50:39 -0500

    Implement `write_items_list_to_file`

---
+++ data_gateway/disk/disk_writer.py
@@ -0,0 +1,11 @@
+def write_items_list_to_file(items_list, file_path):
+    """Writes the list to the file.
+
+    Parameters:
+        items_list (list of str): the list to be stored.
+        file_path (str): the file path.
+    """
+    file = open(file_path, 'w')
+    for item in items_list:
+        file.write(item)
+    file.close()

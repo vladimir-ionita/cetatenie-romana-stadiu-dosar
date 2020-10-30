762925797e5d1bdb2ed38de7f0194ad8fb0c7f2d
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 14:24:16 -0500

    Implement `get_dossiers_collection_file_path_for_year` in `paths`

--- paths/paths.py
+++ paths/paths.py
@@ -75,3 +75,16 @@ def get_order_txt_file_path(order):
     """
     order_file_name = "Ordinul {}.txt".format(order.name)
     return get_publishing_folder_path(order.publishing).joinpath(order_file_name)
+
+
+def get_dossiers_collection_file_path_for_year(year):
+    """Return the dossiers collection file path.
+
+    Parameters:
+        year (int): the dossier collection year
+
+    Return:
+        Path: the dossiers collection file path.
+    """
+    dossiers_collection_file_name = "Dossiers {}.txt".format(year)
+    return get_dossiers_storage_path().joinpath(dossiers_collection_file_name)

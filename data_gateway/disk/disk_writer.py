0a0b0c799752bac36f5708de719d6fe39acff9e9
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-10-30 14:43:46 -0500

    Fix `write_items_list_to_file`

--- data_gateway/disk/disk_writer.py
+++ data_gateway/disk/disk_writer.py
@@ -7,5 +7,5 @@ def write_items_list_to_file(items_list, file_path):
     """
     file = open(file_path, 'w')
     for item in items_list:
-        file.write(item)
+        file.write("{}\n".format(item))
     file.close()

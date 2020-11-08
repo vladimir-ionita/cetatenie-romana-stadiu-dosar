b7563e6bd54b7887d3ac366eb04163624dae06f0
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-11-08 12:15:47 -0600

    Add two regex patterns for the `OrdersParser` package

--- OrdersParser/constants.py
+++ OrdersParser/constants.py
@@ -1,3 +1,4 @@
 ORDER_NUMBER_REGEX = "ORDIN[^0-9]*([\d]+[^0-9]*)"
-DOSSIER_REGEX = "([\d]+)\/.*([\d]{4})"
+DOSSIER_REGEX = "\(([\d]+)\/[^0-9]*([\d]+)\)"
+DOSSIER_REGEX_NO_PARENTHESES = "([\d]+)\/[^0-9]*([\d]+)"
 ORDER_DESCRIPTION = "ORDIN"

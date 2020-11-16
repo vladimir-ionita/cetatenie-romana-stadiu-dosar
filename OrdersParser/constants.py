bc0ba4576a18906e29266ffed3ec7dfa52a20819
Ionita Vladimir <ionita.vladimir@gmail.com>
2020-11-16 17:07:44 -0600

    Change the `DOSSIER_YEAR_MAXIMUM` to get the current year dynamically

--- OrdersParser/constants.py
+++ OrdersParser/constants.py
@@ -1,7 +1,9 @@
+import datetime
+
 ORDER_DESCRIPTION = "ORDIN"
 ORDER_NUMBER_REGEX = "ORDIN[^0-9]*([\d]+[^0-9]*)"
 DOSSIER_REGEX = "\(([\d]+)\/[^0-9]*([\d]+)\)"
 DOSSIER_REGEX_NO_PARENTHESES = "([\d]+)\/[^0-9]*([\d]+)"
 
 DOSSIER_YEAR_MINIMUM = 2000
-DOSSIER_YEAR_MAXIMUM = 2020
+DOSSIER_YEAR_MAXIMUM = datetime.datetime.now().year
